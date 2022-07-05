4c062185c90863704758ed450dbfdccdd5c714b6
DESKTOP-K11B49H\Anh Duc <anhduc@gmail.com>
2022-07-05 23:20:04 +0700

    no message

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ async function main() {
     console.log("Microservices rock!");
 	const username = process.env.DOCKER_USERNAME;
 	console.log('My username is %s', username);
+	console.log(`My username = ${username}`);
     await sleep(5000);
   }
 }
